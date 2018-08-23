d0643c986ad1d1d2dec170171c8ea167b1297dec
Mark Gerasimov <728239@gmail.com>
2018-08-23 23:26:58 +0300

    Добавил контроллер

---
+++ NoSqlLibrary/src/main/java/gerasimov/springdev/nosqllibrary/controller/LibController.java
@@ -0,0 +1,21 @@
+package gerasimov.springdev.nosqllibrary.controller;
+
+import gerasimov.springdev.nosqllibrary.facade.LibraryFacade;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibController {
+
+    private final LibraryFacade libraryFacade;
+
+    public LibController(LibraryFacade libraryFacade) {
+
+        this.libraryFacade = libraryFacade;
+    }
+
+    @GetMapping("/")
+    public String list() {
+        return "list";
+    }
+}
