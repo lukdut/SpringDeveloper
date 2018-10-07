409fed6279d42d064e5d681c55f222fb372f574c
Mark Gerasimov <728239@gmail.com>
2018-10-07 15:39:32 +0300

    Добавлена конфигурация ACL

--- NoSqlLibrary/src/main/java/gerasimov/springdev/nosqllibrary/NoSqlLibraryApplication.java
+++ NoSqlLibrary/src/main/java/gerasimov/springdev/nosqllibrary/NoSqlLibraryApplication.java
@@ -2,8 +2,14 @@ package gerasimov.springdev.nosqllibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@PropertySource("classpath:application.yml")
+@EnableJpaRepositories
 public class NoSqlLibraryApplication {
     //add book --authors author --genres genre
     public static void main(String[] args) {
---
+++ NoSqlLibrary/src/main/resources/acl-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO acl_class (id, class) VALUES
+(1, 'gerasimov.springdev.nosqllibrary.model.Book');
