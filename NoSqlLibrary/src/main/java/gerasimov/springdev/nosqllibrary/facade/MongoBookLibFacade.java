c0653f36b11153cc15df2c0acbd26bd408eb1c18
Mark Gerasimov <728239@gmail.com>
2018-10-10 00:34:09 +0300

    Добавил шаблон работы с аклом

--- NoSqlLibrary/src/main/java/gerasimov/springdev/nosqllibrary/facade/MongoBookLibFacade.java
+++ NoSqlLibrary/src/main/java/gerasimov/springdev/nosqllibrary/facade/MongoBookLibFacade.java
@@ -2,11 +2,23 @@ package gerasimov.springdev.nosqllibrary.facade;
 
 import gerasimov.springdev.nosqllibrary.model.Book;
 import gerasimov.springdev.nosqllibrary.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MongoBookLibFacade implements BookLibFacade {
     final BookRepository bookRepository;
+
+    //Да простят меня боги_вайринга_через_конструктор
+    @Autowired
+    private MutableAclService aclService;
 
     public MongoBookLibFacade(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -29,6 +41,54 @@ public class MongoBookLibFacade implements BookLibFacade {
 
     @Override
     public String addBook(Book book) {
-        return bookRepository.save(book).getId();
+        String id = bookRepository.save(book).getId();
+        // создать SID-ы для владельца и пользователя
+        final Sid owner = new PrincipalSid(SecurityContextHolder.getContext().getAuthentication());
+        final Sid admin = new GrantedAuthoritySid("ROLE_ADMIN");
+        // создать ObjectIdentity для бизнес сущности
+        final ObjectIdentity oid = new ObjectIdentityImpl(Book.class, idFromString(id));
+        // создать пустой ACL
+        final MutableAcl acl = aclService.createAcl(oid);
+
+        // определить владельца сущности и права пользователей
+        acl.setOwner(owner);
+        acl.insertAce(acl.getEntries().size(), BasePermission.READ, owner, true);
+        acl.insertAce(acl.getEntries().size(), BasePermission.ADMINISTRATION, admin, true);
+        // обновить ACL в БД
+        aclService.updateAcl(acl);
+
+        check(id);
+
+        return id;
+    }
+
+    private long idFromString(String value) {
+        long h = 0;
+        if (h == 0 && value.length() > 0) {
+            char val[] = value.toCharArray();
+
+            for (int i = 0; i < value.length(); i++) {
+                h = 31 * h + val[i];
+            }
+        }
+        return h;
+    }
+
+
+    private void check(String id) {
+        //Проверка
+        // создать ObjectIdentity для бизнес сущности
+        final ObjectIdentity oid = new ObjectIdentityImpl(Book.class, idFromString(id));
+        // прочитать ACL бизнес сущности
+        Acl acl = aclService.readAclById(oid);
+        // определить какие права и для кого проверять
+        final List<Permission> permissions = Arrays.asList(BasePermission.READ);
+        final List<Sid> sids = Arrays.asList((Sid) new GrantedAuthoritySid("admin"));
+        // выполнить проверку
+        acl.getOwner();
+        //TODO
+        /*if (!acl.isGranted(permissions, sids, false)) {
+            throw new RuntimeException("Access denied.");
+        }*/
     }
 }
