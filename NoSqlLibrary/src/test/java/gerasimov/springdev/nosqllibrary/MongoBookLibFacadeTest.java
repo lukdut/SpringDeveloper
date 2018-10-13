2300c98298f93f4f6156f1287c2cd6029c7698b4
Mark Gerasimov <728239@gmail.com>
2018-10-13 14:55:58 +0300

    Немного тестов, починены старые

--- NoSqlLibrary/src/test/java/gerasimov/springdev/nosqllibrary/MongoBookLibFacadeTest.java
+++ NoSqlLibrary/src/test/java/gerasimov/springdev/nosqllibrary/MongoBookLibFacadeTest.java
@@ -3,41 +3,32 @@ package gerasimov.springdev.nosqllibrary;
 import gerasimov.springdev.nosqllibrary.facade.MongoBookLibFacade;
 import gerasimov.springdev.nosqllibrary.model.Book;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class MongoBookLibFacadeTest {
     @Autowired
     MongoBookLibFacade bookLibFacade;
     private Book authorsBook = new Book("authors");
 
-    @Before
-    @WithMockUser(username = "author")
-    public void prepare() {
-        bookLibFacade.addBook(authorsBook);
-    }
-
     @Test
     @WithMockUser(username = "admin", authorities = {"ROLE_ADMIN"})
-    public void adminCanCreate() {
+    public void adminCanDelete() {
         String id = bookLibFacade.addBook(new Book("test"));
         Assert.assertNotNull(id);
         bookLibFacade.deleteBook(id);
     }
 
-    @Test
-    @WithMockUser(username = "user")
-    public void userCanNonUpdate() {
-        authorsBook.setTitle("new");
-        bookLibFacade.updateBook(authorsBook);
+    @Test(expected = IllegalArgumentException.class)
+    public void unauthorized() {
+        String id = bookLibFacade.addBook(new Book("test"));
+        Assert.assertNotNull(id);
+        bookLibFacade.deleteBook(id);
     }
 }
