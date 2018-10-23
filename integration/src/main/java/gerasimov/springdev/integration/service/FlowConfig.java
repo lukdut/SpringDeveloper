7e4b1708699f7acc05c15850b23a17f5ac59d100
Mark Gerasimov <728239@gmail.com>
2018-10-23 17:58:31 +0300

    Переделана реализация фильтрации с учетом инфы из след лекции

--- integration/src/main/java/gerasimov/springdev/integration/service/FlowConfig.java
+++ integration/src/main/java/gerasimov/springdev/integration/service/FlowConfig.java
@@ -2,25 +2,16 @@ package gerasimov.springdev.integration.service;
 
 
 import gerasimov.springdev.integration.model.Order;
-import gerasimov.springdev.integration.model.OrderPosition;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.Router;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.router.AbstractMessageRouter;
 import org.springframework.integration.transformer.GenericTransformer;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 
 import static gerasimov.springdev.integration.IntegrationApplication.VIP_THRESHOLD;
 import static gerasimov.springdev.integration.service.ChannelsConfig.*;
@@ -35,45 +26,29 @@ public class FlowConfig {
     }
 
     @Bean
-    @Router(inputChannel = NON_EMPTY_ORDERS)
-    public AbstractMessageRouter correctnessRouter(
-            @Qualifier("correctOrders") MessageChannel ok,
-            @Qualifier("incorrectOrders") MessageChannel incorrect) {
-        return new AbstractMessageRouter() {
-            @Override
-            protected Collection<MessageChannel> determineTargetChannels(Message<?> message) {
-                final Order payload = (Order) message.getPayload();
-                final Optional<OrderPosition> negativeAmount = payload.getPositions().stream()
-                        .filter(orderPosition -> orderPosition.getPrice() <= 0 || orderPosition.getCount() <= 0)
-                        .findAny();
-                if (negativeAmount.isPresent()) {
-                    return Collections.singletonList(incorrect);
-                } else {
-                    return Collections.singletonList(ok);
-                }
-            }
-        };
+    public IntegrationFlow correctnessFlow() {
+        return f -> f.channel(NON_EMPTY_ORDERS)
+                .<Order, Boolean>route(
+                        order -> order.getPositions().stream()
+                                .filter(orderPosition -> orderPosition.getPrice() <= 0 || orderPosition.getCount() <= 0)
+                                .findAny().isPresent(),
+                        mapping -> {
+                            mapping.subFlowMapping(true, sf -> sf.channel(INCORRECT_ORDERS))
+                                    .subFlowMapping(false, sf -> sf.channel(CORRECT_ORDERS));
+                        });
     }
 
     @Bean
-    @Router(inputChannel = CORRECT_ORDERS)
-    public AbstractMessageRouter vipRouter(
-            @Qualifier("ordinaryOrders") MessageChannel ordinary,
-            @Qualifier("vipOrders") MessageChannel vip) {
-        return new AbstractMessageRouter() {
-            @Override
-            protected Collection<MessageChannel> determineTargetChannels(Message<?> message) {
-                final Order payload = (Order) message.getPayload();
-                double totalAmount = payload.getPositions().stream()
-                        .mapToDouble(orderPosition -> orderPosition.getPrice() * orderPosition.getCount())
-                        .sum();
-                if (totalAmount >= VIP_THRESHOLD) {
-                    return Collections.singletonList(vip);
-                } else {
-                    return Collections.singletonList(ordinary);
-                }
-            }
-        };
+    public IntegrationFlow vipOrdersFlow() {
+        return f -> f.channel(CORRECT_ORDERS)
+                .<Order, Boolean>route(
+                        order -> order.getPositions().stream()
+                                .mapToDouble(orderPosition -> orderPosition.getPrice() * orderPosition.getCount())
+                                .sum() >= VIP_THRESHOLD,
+                        mapping -> {
+                            mapping.subFlowMapping(true, sf -> sf.channel(VIP_ORDERS))
+                                    .subFlowMapping(false, sf -> sf.channel(ORDINARY_ORDERS));
+                        });
     }
 
     @Bean
